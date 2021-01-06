967444f558bf1fea2ca30cdb2381c03476c0dc89
Tempest Campbell <TempestCampbell@hotmail.com>
2021-01-05 21:14:58 -0700

    API keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6acd3e1ec469c421bbb53dd7b0acaa1d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCfFje9Kbm2NoPIDCm7dr3IxtniVLZBwmQ"
